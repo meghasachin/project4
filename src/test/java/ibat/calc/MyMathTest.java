cb57ac4d4a6c42b45616483985abbda770879c5f
Megha Sachin <meghahadke@yahoo.in>
2021-08-08 16:28:55 +0100

    wrote test class for calculator

---
+++ src/test/java/ibat/calc/MyMathTest.java
@@ -0,0 +1,26 @@
+package ibat.calc;
+
+import org.junit.Test;
+
+public class MyMathTest {
+	
+	@Test
+	public void testAdd() {
+		
+	}
+	
+	@Test
+	public void testSubtract() {
+		
+	}
+	
+	@Test
+	public void testMultiply() {
+		
+	}
+	
+	@Test
+	public void testDivide() {
+		
+	}
+}
