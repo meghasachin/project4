3f2170ffb92049b89004387bb3a0f367528197aa
Megha Sachin <meghahadke@yahoo.in>
2021-08-08 16:40:14 +0100

    failing test cases written for calculator

---
+++ src/main/java/ibat/calc/MyMath.java
@@ -0,0 +1,21 @@
+package ibat.calc;
+
+
+public class MyMath {
+	
+	public int testAdd(int a, int b) {
+		return 0;
+	}
+	
+	public int testSubtract(int a, int b) {
+		return 0;
+	}
+	
+	public int testMultiply(int a, int b) {
+		return 0;
+	}
+	
+	public int testDivide(int a, int b) {
+		return 0;
+	}
+}
--- src/test/java/ibat/calc/MyMathTest.java
+++ src/test/java/ibat/calc/MyMathTest.java
@@ -1,26 +1,48 @@
 package ibat.calc;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyMathTest {
 	
-	@Test
-	public void testAdd() {
-		
+	private MyMath testObject;
+	
+	@Before
+	public void initialize() {
+		testObject = new MyMath();
 	}
 	
 	@Test
-	public void testSubtract() {
-		
+	public void testAdd_TC1() {
+		int a = 10;
+		int b = 20;
+		int result = testObject.testAdd(a, b);
+		assertEquals("Addition of 10 and 20 is.", 30, result);
+	}
+	
+	@Test
+	public void testSubtract_TC1() {
+		int a = 20;
+		int b = 10;
+		int result = testObject.testSubtract(a, b);
+		assertEquals("Substraction of 20 and 10 is.", 10, result);
 	}
 	
 	@Test
 	public void testMultiply() {
-		
+		int a = 10;
+		int b = 20;
+		int result = testObject.testMultiply(a, b);
+		assertEquals("Multiplication of 10 and 20 is.", 200, result);
 	}
 	
 	@Test
 	public void testDivide() {
-		
+		int a = 20;
+		int b = 10;
+		int result = testObject.testDivide(a, b);
+		assertEquals("Multiplication of 20 and 10 is.", 2, result);
 	}
 }
