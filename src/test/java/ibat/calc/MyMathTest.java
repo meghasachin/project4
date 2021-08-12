df6f2dadb0c0ffd3b64eb781d289686e96f166c1
Megha Sachin <meghahadke@yahoo.in>
2021-08-13 00:22:09 +0100

    updated test cases for subtract - all working

--- src/test/java/ibat/calc/MyMathTest.java
+++ src/test/java/ibat/calc/MyMathTest.java
@@ -63,46 +63,50 @@ public class MyMathTest {
 	}	
 
 	// test subtraction of positive whole numbers
-//	@Test
-//	public void testSubtract_TC1() {
-//		int a = 20;
-//		int b = 10;
-//		float result = testObject.testSubtract(a, b);
-//		assertEquals("Substraction of 20 and 10 is.", 10, result, 0);
-//	}
-//	
-//	@Test
-//	public void testSubtract_TC2() {
-//		double a = 5.5;
-//		double b = 4.4;
-//		double result = testObject.testSubtract(a, b);
-//		assertEquals("Substraction of 5.5 and 4.4 is.", 1.1, result, 0.1);
-//	}
-//	
-//	@Test
-//	public void testSubtract_TC3() {
-//		double a = 5.5;
-//		double b = -4.4;
-//		double result = testObject.testSubtract(a, b);
-//		assertEquals("Substraction of 5.5 and -4.4 is.", 10, result, 0.1);
-//	}
-//	
-//	@Test
-//	public void testSubtract_TC4() {
-//		double a = -5.5;
-//		double b = -4.4;
-//		double result = testObject.testSubtract(a, b);
-//		assertEquals("Substraction of -5.5 and -4.4 is.", 1.1, result, -0.1);
-//	}
-//	
-//	@Test
-//	public void testSubtract_TC5() {
-//		double a = 0;
-//		double b = 0;
-//		double result = testObject.testSubtract(a, b);
-//		assertEquals("Substraction of 0 and 0 is.", 0, result, 0);
-//	}	
-//	
+	@Test
+	public void testSubtract_TC1() {
+		BigDecimal a = new BigDecimal(20);
+		BigDecimal b = new BigDecimal(10);
+		float result = testObject.subtract(a, b);
+		assertEquals("Substraction of 20 and 10 is.", new BigDecimal(10).floatValue(), result, 0.0);
+	}
+	
+	// test subtraction of positive decimal numbers
+	@Test
+	public void testSubtract_TC2() {
+		BigDecimal a = new BigDecimal(5.5);
+		BigDecimal b = new BigDecimal(4.4);
+		double result = testObject.subtract(a, b);
+		assertEquals("Substraction of 5.5 and 4.4 is.", new BigDecimal(1.1).floatValue(), result, 0.0);
+	}
+	
+	// test subtraction of positive and negative decimal numbers
+	@Test
+	public void testSubtract_TC3() {
+		BigDecimal a = new BigDecimal(5.5);
+		BigDecimal b = new BigDecimal(-4.5); 
+		float result = testObject.subtract(a, b);
+		assertEquals("Substraction of 5.5 and -4.5 is.", new BigDecimal(10).floatValue(), result, 0.0);
+	}
+	
+	// test subtraction of both negative decimal numbers
+	@Test
+	public void testSubtract_TC4() {
+		BigDecimal a = new BigDecimal(-5.5);
+		BigDecimal b = new BigDecimal(-4.4);
+		float result = testObject.subtract(a, b);
+		assertEquals("Substraction of -5.5 and -4.4 is.", new BigDecimal(-1.1).floatValue(), result, 0.0);
+	}
+	
+	// test subtraction of zeros
+	@Test
+	public void testSubtract_TC5() {
+		BigDecimal a = new BigDecimal(0);
+		BigDecimal b = new BigDecimal(0);
+		float result = testObject.subtract(a, b);
+		assertEquals("Substraction of 0 and 0 is.", new BigDecimal(0).floatValue(), result, 0.0);
+	}	
+	
 //	@Test
 //	public void testMultiply() {
 //		int a = 10;
